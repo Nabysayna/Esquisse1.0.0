70a1ddddaa6a96f10ff12ad844f1b03be9df1b4f
HDabdou <abdouhmd31@gmail.com>
2018-10-26 17:53:54 +0000

    26/10/2018-1

--- src/app/services/canal.service.ts
+++ src/app/services/canal.service.ts
@@ -4,7 +4,7 @@ import {Http, Headers} from "@angular/http";
 @Injectable()
 export class CanalService {
 
- private link = "http://localhost/canal_backend/";
+ private url = "http://localhost/canal_backend/";
 
   private headers=new Headers();
   private token : string = JSON.parse(sessionStorage.getItem('currentUser')).baseToken ;
@@ -12,7 +12,7 @@ export class CanalService {
 
   public abonnement(requete:any,nom,prenom,tel,NumAbonner,NumDecoudeur,numCarte,Formule,prix,nombreMois): Promise<any>{
     let params="requestParam="+JSON.stringify({requete : requete,nom:nom,prenom:prenom,tel:tel,NumAbonner:NumAbonner,NumDecoudeur:NumDecoudeur,numCarte:numCarte,Formule:Formule,prix:prix,nombreMois:nombreMois});
-    let link=this.link+"handleAlert.php";
+    let link=this.url+"handleAlert.php";
     console.log(params);
     return this.http.post(link,params,{headers:this.headers}).toPromise().then( res => {console.log(res);return res} ).catch(error => {console.log(error);return 'bad' });
   }
